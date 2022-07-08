0da5928e003b7a6d6a5d020b3510ff8bb36934f4
oneren37 <oneren37@gmail.com>
2022-07-08 15:42:20 +0300

    feat(server): конфиг webpack

---
+++ server/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  mode: 'production',
+  target: 'node',
+  entry: {
+    main: path.resolve(__dirname, './src/index.tsx'),
+  },
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  externals: [nodeExternals()],
+  externalsPresets: {
+    node: true,
+  },
+};
